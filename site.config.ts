b66041536c31e48f89015512f6df8fd96175d03e
Bridger Tower <bridgertower@gmail.com>
2025-12-10 12:28:21 -0700

    update to next-woo

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ type SiteConfig = {
 };
 
 export const siteConfig: SiteConfig = {
-  site_name: "next-wp",
-  site_description: "Starter template for Headless WordPress with Next.js",
-  site_domain: "https://next-wp.com",
+  site_name: "next-woo",
+  site_description: "Headless WooCommerce store powered by Next.js",
+  site_domain: "https://next-woo.com",
 };
